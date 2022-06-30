58572ca2e91fc11317a43c34f71e7ac95fca099c
yshr,fuk <steptime0226@gmail.com>
2022-06-30 19:03:20 +0900

    ♻️ Linksのリファクタリング

--- src/components/Links/index.jsx
+++ src/components/Links/index.jsx
@@ -3,7 +3,7 @@ import classes from "src/components/Links/Links.module.css";
 export const Links = (props) => {
   return (
     <div className={classes.grid}>
-      {props.map((item) => {
+      {props.items.map((item) => {
         return (
           <a key={item.href} href={item.href} className={classes.card}>
             <h2 className={classes.title}>{item.title}</h2>
